f450b579ba2bcee2e5962617244d6b1917c0eb32
Luisdavidpm <alu0100816518@ull.edu.es>
2016-10-06 23:17:32 +0100

    Añadiendo archivos al de la practica

---
+++ Interface/Assets/Standard Assets/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col){
+
+
+		if (col.gameObject.name == "FPSController"){
+			GetComponent<Renderer>().material.color = Random.ColorHSV();	
+		}
+	}
+}
