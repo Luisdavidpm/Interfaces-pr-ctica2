49dc09d63ad7fe15719f1eb3172ae9a1f5e3e66e
Luisdavidpm <alu0100816518@ull.edu.es>
2016-10-07 13:22:13 +0100

    Añadiendo delegacion

--- Interface/Assets/Standard Assets/NewBehaviourScript.cs
+++ Interface/Assets/Standard Assets/NewBehaviourScript.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
 
+	public delegate void metodoDelegado();
+	public static metodoDelegado Evento;
+
 	void OnCollisionEnter(Collision col){
-
-
-		if (col.gameObject.name == "FPSController"){
-			GetComponent<Renderer>().material.color = Random.ColorHSV();	
-		}
+		Evento ();
 	}
+
 }
